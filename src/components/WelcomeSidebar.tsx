a62053f27a69ae168ba59d1923daddae04d98384
Moritz Ruehm <Moritz.Ruehm@osapiens.com>
2023-12-18 17:10:20 +0100

    configure welcomesidebar

--- src/components/WelcomeSidebar.tsx
+++ src/components/WelcomeSidebar.tsx
@@ -1,10 +1,14 @@
-import { Drawer, Typography } from "@mui/material";
+import { Box, Drawer, Typography } from "@mui/material";
 
 function WelcomeSidebar() {
   return (
     <Drawer variant="permanent">
-      <img src="https://via.placeholder.com/150" alt="Logo" />
-      <Typography variant="h6">Marmitt Corp.</Typography>
+      <Box bgcolor={"#6857E9"} width={"100%"} height={"100%"}>
+      <img src="globe-white.svg" alt="Logo" width={200} style={{margin: 20}} />
+      <Typography variant="h6" textAlign={"center"}>Marmitt Corp.</Typography>
+      {/* Copyright 2023, Marmitt Corporation. All rights reserved */}
+      <Typography variant="subtitle1" textAlign={"center"}></Typography>
+      </Box>
     </Drawer>
   );
 }
