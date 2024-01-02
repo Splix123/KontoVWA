e8fea3353ed065cc468003bd1ccc1678012f3fc6
Splix123 <moe.ruehm@googlemail.com>
2024-01-02 13:26:03 +0100

    changes

--- src/components/AddBuchungDialog.tsx
+++ src/components/AddBuchungDialog.tsx
@@ -1,4 +1,3 @@
-// Libraries
 import {
   Button,
   Dialog,
@@ -6,133 +5,51 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Fab,
   TextField,
 } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
-import dayjs, { Dayjs } from "dayjs";
-import { useState } from "react";
-import { useMutation } from "react-query";
 
-// Stores
-import buchungStore from "../store/buchungStore.store";
-
-// Types
-import { Buchung } from "../../types.d";
-type Props = {
-  kontonummer: string;
-};
-
-// Icons
-import { Add } from "@mui/icons-material";
-
-// Functions
-async function addBuchungFunction(buchung: Buchung) {
-  const response = await fetch(`http://localhost:3000/buchung`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(buchung),
-  });
-  return response.json();
-}
-
-function AddBuchung({ kontonummer }: Props) {
-  //Mutations
-  const { mutateAsync: addBuchungMutation } = useMutation({
-    mutationFn: addBuchungFunction,
-  });
-
-  // States
-  const { buchungen, addBuchung, lastId } = buchungStore();
-  const [openDialog, setOpenDialog] = useState(false);
-  const [betrag, setBetrag] = useState("");
-  const [error, setError] = useState(false);
-  const [datum, setDatum] = useState<Dayjs | null>(dayjs());
-  const [text, setText] = useState("");
-
-  // Handler
-  const handleClickAddBuchung = () => {
-    setOpenDialog(true);
-  };
-
-  const handleAddBuchung = () => {
-    if (betrag === "") {
-      setError(true);
-    } else {
-      const newBuchung = {
-        id: lastId + 1,
-        lfd: buchungen.length + 1,
-        betrag: Number(betrag),
-        buchungsdatum: dayjs(datum).format("DD.MM.YYYY"),
-        buchungstext: text,
-        kontonummer: kontonummer,
-      };
-      addBuchungMutation(newBuchung);
-      addBuchung(newBuchung);
-      setOpenDialog(false);
-    }
-  };
-
-  const handleClose = () => {
-    setOpenDialog(false);
-    setError(false);
-  };
-
+function AddBuchung() {
   return (
-    <>
-      <Fab
-        color="primary"
-        variant="extended"
-        aria-label="add"
-        style={{ marginRight: 20 }}
-        onClick={handleClickAddBuchung}
-      >
-        <Add />
-        Buchung hinzufügen
-      </Fab>
-      <Dialog open={openDialog} onClose={handleClose}>
-        <DialogTitle fontWeight="bold">Neue Buchung</DialogTitle>
-        <DialogContent>
-          <DialogContentText color="text.primary">
-            Geben Sie ihre Buchungsdaten bitte hier ein
-          </DialogContentText>
-          <TextField
-            required
-            error={error}
-            autoFocus
-            margin="normal"
-            id="betrag"
-            label="Betrag"
-            type="number"
-            fullWidth
-            variant="outlined"
-            placeholder="z.B. 500 oder -500"
-            onChange={(e) => {
-              setBetrag(e.target.value);
-            }}
-          />
-          <DatePicker
-            value={datum}
-            onChange={(newDatum) => setDatum(newDatum)}
-          />
-          <TextField
-            margin="normal"
-            id="text"
-            label="Buchungstext"
-            type="text"
-            fullWidth
-            variant="outlined"
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Abbrechen</Button>
-          <Button onClick={handleAddBuchung}>Buchen</Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={openDialog} onClose={handleClose}>
+      <DialogTitle fontWeight="bold">Neue Buchung</DialogTitle>
+      <DialogContent>
+        <DialogContentText color="text.primary">
+          Geben Sie ihre Buchungsdaten bitte hier ein
+        </DialogContentText>
+        <TextField
+          required
+          error={error}
+          autoFocus
+          margin="normal"
+          id="betrag"
+          label="Betrag"
+          type="number"
+          fullWidth
+          variant="outlined"
+          placeholder="z.B. 500 oder -500"
+          onChange={(e) => {
+            setBetrag(e.target.value);
+          }}
+        />
+        <DatePicker value={datum} onChange={(newDatum) => setDatum(newDatum)} />
+        <TextField
+          margin="normal"
+          id="text"
+          label="Buchungstext"
+          type="text"
+          fullWidth
+          variant="outlined"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Abbrechen</Button>
+        <Button onClick={handleAddBuchung}>Buchen</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
